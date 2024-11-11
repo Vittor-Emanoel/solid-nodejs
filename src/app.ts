2ad6258aee3e6714d85f82af8de7af6004a018df
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-11-10 23:27:24 -0300

    feat: create first router

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,25 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
 
 export const app = fastify();
 
-const prisma = new PrismaClient({
-  log: ["query"],
-});
+app.post("/users", async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string().min(1),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
 
-prisma.user.create({
-  data: {
-    name: "ew",
-    email: "awdwad",
-  },
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  });
+
+  return reply.status(201).send();
 });
