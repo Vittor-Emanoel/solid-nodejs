ffb4e44da890c9d9b148ebf7547ef2476b687e3e
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-11-10 22:52:11 -0300

    chore: add prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
 export const app = fastify();
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+
+prisma.user.create({
+  data: {
+    name: "ew",
+    email: "awdwad",
+  },
+});
