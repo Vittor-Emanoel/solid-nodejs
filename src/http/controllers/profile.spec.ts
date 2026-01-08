c71489aac03ea67fa4b6957b12710e6f3c5e2f12
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2026-01-07 22:01:44 -0300

    test: e2e profile user

---
+++ src/http/controllers/profile.spec.ts
@@ -0,0 +1,39 @@
+import { app } from "@/app";
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+describe("Profile (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to get user profile", async () => {
+    await request(app.server).post("/users").send({
+      name: "john doe",
+      email: "john@example.com",
+      password: "123456",
+    });
+
+    const authResponse = await request(app.server).post("/sessions").send({
+      email: "john@example.com",
+      password: "123456",
+    });
+
+    const { accessToken } = authResponse.body;
+
+    const profileResponse = await request(app.server)
+      .get("/me")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send();
+
+    expect(profileResponse.statusCode).toEqual(200);
+    expect(profileResponse.body.user).toEqual(
+      expect.objectContaining({
+        email: "john@example.com",
+      }),
+    );
+  });
+});
